ae4a094c874b7c2768e2a372eccb7d67338a97c0
orris27 <gsorris27@gmail.com>
2019-05-18 16:14:29 +0800

    remove print infos

--- model.py
+++ model.py
@@ -37,7 +37,6 @@ class Model(torch.nn.Module):
         x = torch.Tensor(x).to(device)
         y = torch.Tensor(y).to(device)
 
-        #output_list, final_state = self.stacked_cell(x)
         output_list, final_state = self.stacked_cell(x, None)
 
         output = self.fc_output(output_list.reshape(-1, self.args.rnn_state_size)) # (batch_size * args.T, self.NOUT=121)
@@ -82,9 +81,7 @@ class Model(torch.nn.Module):
 
         final_state = None
         for i in range(length - 1):
-        
-            #output_list, final_state = self.stacked_cell(torch.Tensor(x).to(device))
-            output_list, final_state = self.stacked_cell(torch.Tensor(x).to(device), final_state)
+            output_list, final_state = self.stacked_cell(torch.Tensor(x).to(device), final_state) # !!! The final state argument is important because the PyTorch LSTM would initialize its states otherwise. Hence, we suggest that we should alwayes call LSTM with its initial states. None represents the empty states.
             output = self.fc_output(output_list.reshape(-1, self.args.rnn_state_size)) # (1, NOUT:121)
             end_of_stroke = 1 / (1 + torch.exp(output[:, 0])) # (1, )
             pi_hat, mu1, mu2, sigma1_hat, sigma2_hat, rho_hat = torch.split(output[:, 1:], self.args.M, 1)
